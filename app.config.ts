ea5f4eee0f6982cacecda8cf90d062c4cddd3897
thepiguy <kunal.a.dandekar@gmail.com>
2023-01-28 12:36:24 +0100

    initial code for the index

--- app.config.ts
+++ app.config.ts
@@ -1,28 +1,30 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: "TCore",
+    description:
+      "TCore is a virtual platform that enables the developer to focus on their app",
+    // image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_themes',
-      github: 'nuxt-themes/docus',
+      // twitter: 'nuxt_themes',
+      github: "TecTone23",
     },
     aside: {
       level: 0,
-      exclude: []
+      exclude: [],
     },
     header: {
       logo: true,
       showLinkIcon: true,
-      exclude: []
+      exclude: [],
     },
     footer: {
-      iconLinks: [
-        {
-          href: 'https://nuxt.com',
-          icon: 'IconNuxtLabs'
-        }
-      ]
+      fluid: true,
+    },
+    github: {
+      edit: true,
+      repo: "tcore-docs",
+      branch: "main",
+      owner: "TecTone23"
     }
-  }
-})
+  },
+});
