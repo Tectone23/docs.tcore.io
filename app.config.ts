f0a5015dd6290eddcde14e24ba9c9f85090a5351
thepiguy <kunal.a.dandekar@gmail.com>
2023-02-10 21:04:03 +0100

    final changes before going live

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ export default defineAppConfig({
     title: "TCore",
     description:
       "TCore is a virtual platform that enables the developer to focus on their app",
-    // image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    image: '',
     socials: {
       // twitter: 'nuxt_themes',
       github: "TecTone23",
@@ -23,6 +23,7 @@ export default defineAppConfig({
     },
     footer: {
       fluid: true,
+      credits: false
     },
     github: {
       edit: true,
