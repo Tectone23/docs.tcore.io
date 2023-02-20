0b976da1e1444309fc021b2a2c9aed624d0854da
thepiguy <kunal.a.dandekar@gmail.com>
2023-02-20 17:52:26 +0100

    Minor changes

---
+++ components/content/Hidden.vue
@@ -0,0 +1,14 @@
+<template>
+  <p class="hidden">Answer = Exponential</p>
+</template>
+
+<style scoped lang="ts">
+css({
+  '.hidden': {
+    color: "#fdf6db",
+    '@dark': {
+      color: "#362b03",
+    }
+  }
+})
+</style>
